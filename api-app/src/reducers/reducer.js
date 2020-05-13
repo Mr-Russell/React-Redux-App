7ab362e05b680f289c3889e8f07f1420e3ae1d38
Brian Russell <bruss@brians-mbp.lan>
2020-05-13 19:08:32 -0400

    Built skeleton structure of app

---
+++ api-app/src/reducers/reducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+  isFetching: false,
+  joke: null,
+  error: ''
+}
+
+export const reducer = (state = initialState, action) => {
+  switch(action.type){
+    default:
+      return state
+  }
+}
